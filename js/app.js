c941dd3c9f25dd03f651c96f9fa66baec2cc08dc
shohan-cnits <shohan.cnits@gmail.com>
2024-01-21 15:29:37 +0600

    move to github

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
